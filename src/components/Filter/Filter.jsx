daf4df8474aeaa4fbe98a467e3e422f1ed8f15d2
hitmen798 <124578265+hitmen798@users.noreply.github.com>
2023-12-11 17:50:03 +0100

    123

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import style from './Filter.module.css';
+
+const Filter = ({ value, onChange }) => (
+  <div className={style.searchContainer}> {/* Додаємо обгортку для центрування */}
+    <label className={style.search}>
+      <input
+        type="text"
+        name="filter"
+        placeholder=" "
+        className={style.inputName}
+        title="Enter search name"
+        onChange={onChange}
+        value={value}
+      />
+      <button type="reset"></button>
+    </label>
+  </div>
+);
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Filter;
